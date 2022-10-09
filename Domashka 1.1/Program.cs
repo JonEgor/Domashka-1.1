8e327ef53d9b28130f2746e3f879ccffd5ffb9e1
JonEgor <114680645+JonEgor@users.noreply.github.com>
2022-10-09 23:42:49 +0300

    Fininish version 1.5.1

--- Domashka 1.1/Program.cs
+++ Domashka 1.1/Program.cs
@@ -4,3 +4,10 @@ Console.WriteLine("Hello, it dont world");
 Console.WriteLine("Hello Worold 1.3");
 
 Console.WriteLine("Project, world this my homework!");
+
+Console.WriteLine("Add info 1.5 ");
+Console.WriteLine("Add update 1.5.1 ");
+Console.WriteLine("Finish version 1.5.1");
+
+
+
